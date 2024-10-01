328230430e57b2a5c2d149c7a27c910cad5dadaa
carlhavoc <mkowalsky87@gmail.com>
2024-10-01 13:57:13 -0700

    add cell class and draw function

--- graphics.py
+++ graphics.py
@@ -50,3 +50,31 @@ class Line:
             fill=fill_color,
             width=2,
         )
+
+
+class Cell:
+
+    def __init__(self, x1, x2, y1, y2, window) -> None:
+        self.has_right_wall = True
+        self.has_left_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.__win = window
+
+    def draw(self):
+        if self.has_right_wall:
+            line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line, "black")
+        if self.has_left_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(line, "black")
+        if self.has_top_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line, "black")
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from graphics import Line, Point, Window
+from graphics import Cell, Line, Point, Window
 
 win = Window(800, 500)
 point1 = Point(0, 0)
 point2 = Point(400, 400)
 line1 = Line(point1, point2)
-win.draw_line(line1, "black")
+cell = Cell(100, 200, 100, 200, win)
+cell.has_top_wall = False
+cell.draw()
+
+cell2 = Cell(50, 100, 50, 100, win)
+cell2.has_bottom_wall = False
+
+cell2.draw()
 win.wait_for_close()
