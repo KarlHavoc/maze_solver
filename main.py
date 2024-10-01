389607a29b158a648e810a018432c03015314e46
carlhavoc <mkowalsky87@gmail.com>
2024-10-01 14:00:50 -0700

    changed cell init

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ win = Window(800, 500)
 point1 = Point(0, 0)
 point2 = Point(400, 400)
 line1 = Line(point1, point2)
-cell = Cell(100, 200, 100, 200, win)
+cell = Cell(win)
 cell.has_top_wall = False
-cell.draw()
+cell.draw(100, 200, 100, 200)
 
-cell2 = Cell(50, 100, 50, 100, win)
+cell2 = Cell(win)
 cell2.has_bottom_wall = False
 
-cell2.draw()
+cell2.draw(50, 100, 50, 100)
 win.wait_for_close()
