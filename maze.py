f7726cd7ed6e9235371c2740352bf0e40a5a65f7
carlhavoc <mkowalsky87@gmail.com>
2024-10-17 15:14:35 -0700

    added funtionality to solve the maze and backtrack with a different color if at a dead end

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
-from cell import Cell
 import random
 import time
+
+from cell import Cell
 
 
 class Maze:
@@ -55,7 +56,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.05)
+        time.sleep(0.02)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
@@ -117,6 +118,51 @@ class Maze:
         for col in self._cells:
             for cell in col:
                 cell.visited = False
+
+    def solve(self):
+        if self._solve_r(0, 0):
+            return True
+        return False
+            
+
+    def _solve_r(self, i, j):
+        self._animate()
+        self._cells[i][j].visited = True
+        # return True if current cell is bottom right cell
+        if self._cells[i][j] == self._cells[self._num_cols-1][self._num_rows-1]:
+            
+            return True
+        # right
+        if i < self._num_cols - 1 and not self._cells[i+1][j].visited and not self._cells[i][j].has_right_wall:
+            self._cells[i][j].draw_move(self._cells[i+1][j])
+            if self._solve_r(i+1, j):
+                return True
+            self._cells[i][j].draw_move(self._cells[i+1][j], undo=True)
+
+        # left
+        if i > 0 and not self._cells[i][j].has_left_wall and not self._cells[i-1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i-1][j])
+            if self._solve_r(i-1, j):
+                return True
+            self._cells[i][j].draw_move(self._cells[i-1][j], undo=True)
+        # down
+        if j < self._num_rows- 1 and not self._cells[i][j].has_bottom_wall and not self._cells[i][j+1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j+1])
+            if self._solve_r(i, j+1):
+                return True
+            
+            self._cells[i][j].draw_move(self._cells[i][j+1], undo=True)
+        # up
+        if j > 0 and not self._cells[i][j].has_top_wall and not self._cells[i][j-1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j-1])
+            if self._solve_r(i, j-1):
+                return True
+            
+            self._cells[i][j].draw_move(self._cells[i][j-1], undo=True)
+        
+        return False
+        
+
 
 # import random
 # import time
